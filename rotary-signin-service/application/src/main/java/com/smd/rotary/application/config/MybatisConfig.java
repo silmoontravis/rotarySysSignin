229fd5e2c5cd48f15dd30753e05ebc3e4bca0981
neobradley <vic.william2005@gmail.com>
2021-07-08 04:40:22 +0800

    fix: properties

--- rotary-signin-service/application/src/main/java/com/smd/rotary/application/config/MybatisConfig.java
+++ rotary-signin-service/application/src/main/java/com/smd/rotary/application/config/MybatisConfig.java
@@ -67,7 +67,7 @@ public class MybatisConfig {
         Interceptor[] plugins = new Interceptor[1];
         plugins[0] = paginationInterceptor();
         sqlSessionFactory.setPlugins(plugins);
-
+        log.warn("Connecting : {}", masterJdbcUrl);
         return sqlSessionFactory.getObject();
     }
 
